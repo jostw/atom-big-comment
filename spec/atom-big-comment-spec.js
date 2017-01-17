a9a7c719b05e2f85af4cdd936b2ad9720c08c98f
jos <mail@jos.tw>
2017-01-17 15:33:09 +0800

    Remove modal panel and view

--- spec/atom-big-comment-spec.js
+++ spec/atom-big-comment-spec.js
@@ -16,11 +16,7 @@ describe('AtomBigComment', () => {
   });
 
   describe('when the atom-big-comment:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.atom-big-comment')).not.toExist();
-
+    it('should add some test', () => {
       // This is an activation event, triggering it will cause the package to be
       // activated.
       atom.commands.dispatch(workspaceElement, 'atom-big-comment:toggle');
@@ -29,45 +25,7 @@ describe('AtomBigComment', () => {
         return activationPromise;
       });
 
-      runs(() => {
-        expect(workspaceElement.querySelector('.atom-big-comment')).toExist();
-
-        let atomBigCommentElement = workspaceElement.querySelector('.atom-big-comment');
-        expect(atomBigCommentElement).toExist();
-
-        let atomBigCommentPanel = atom.workspace.panelForItem(atomBigCommentElement);
-        expect(atomBigCommentPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'atom-big-comment:toggle');
-        expect(atomBigCommentPanel.isVisible()).toBe(false);
-      });
-    });
-
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.atom-big-comment')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'atom-big-comment:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let atomBigCommentElement = workspaceElement.querySelector('.atom-big-comment');
-        expect(atomBigCommentElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'atom-big-comment:toggle');
-        expect(atomBigCommentElement).not.toBeVisible();
-      });
+      runs(() => {});
     });
   });
 });
